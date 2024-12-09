33097462891df598fed233614ce86a211374aef3
Joaoo <146839436+ooaoJ@users.noreply.github.com>
2024-12-09 16:44:16 -0300

    Update date.js

--- js/date.js
+++ js/date.js
@@ -5,7 +5,10 @@ let anoAtual = dataAtual.getFullYear();
 console.log(diaAtual, mesAtual)
 
 let dateTxt = document.getElementById('date');
+let dateTxt2 = document.getElementById('date2');
 dateTxt.innerHTML = `${diaAtual}/${mesAtual}/${anoAtual}`
+dateTxt2.innerHTML = `${diaAtual}/${mesAtual}/${anoAtual}`
+
 
 let username = 'Admin';
 document.getElementById('nomeUsuario').innerHTML = username;
